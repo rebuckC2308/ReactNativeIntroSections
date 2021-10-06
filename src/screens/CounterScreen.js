f52b89f9b619a67450be57cbfb731601743dcae6
Chris Rebuck <rebuckc2308@gmail.com>
2021-10-06 14:28:38 -0500

    completed CounterScreen and basic understanding of managing state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -5,7 +5,20 @@ const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count:{counter}</Text>
     </View>
   );
 };
